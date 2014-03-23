06fda1dbb021d0a9bd7d338bfa6633a0e3352812
Teng Li <otec.teng@gmail.com>
2014-03-23 23:43:39 +0800

    fix unicorn listen bug in config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,7 +13,6 @@ pid "#{Rails.root}/tmp/pids/unicorn.pid"
 stderr_path "#{Rails.root}/log/unicorn.log"
 stdout_path "#{Rails.root}/log/unicorn.log"
 
-listen 5000, :tcp_nopush => false
 
 listen "/tmp/weebill.sock"
 worker_processes 2
