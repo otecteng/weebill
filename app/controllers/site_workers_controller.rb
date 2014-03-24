ca18c7edf5a904808fc0ef0572a8594e2c3a6dfb
Teng Li <otec.teng@gmail.com>
2014-03-24 21:48:19 +0800

    add simple wx response

--- app/controllers/site_workers_controller.rb
+++ app/controllers/site_workers_controller.rb
@@ -1,7 +1,16 @@
 class SiteWorkersController < ApplicationController
+  def index
+    render :text => params[:echostr]
+  end
+	
+  def create
+  	@content = "hello site!"
+    render :text,:formats => :xml   
+  end
 
-	def index 
-		render json: Site.all
-	end
-
+  private
+  def check_weixin_legality
+    array = ['gentek', params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  end	
 end
--- app/controllers/tb_customers_controller.rb
+++ app/controllers/tb_customers_controller.rb
@@ -1,7 +1,20 @@
 class TbCustomersController < ApplicationController
 
-	def index 
-		render json: "hey,you guys"
-	end
+  def index
+    render :text => params[:echostr]
+  end
 	
+  def create
+  	@content = "hello c!"
+    render :text,:formats => :xml   
+  end
+
+  private
+  def check_weixin_legality
+    array = ['gentek', params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  end	
 end
+
+
+
