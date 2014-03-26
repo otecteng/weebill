6318e52cb8b859cb3ebddb06ddd1f6495b9ab2fa
root <root@wjj-virtual-machine.(none)>
2014-03-26 15:26:20 +0800

    download media

--- app/controllers/site_workers_controller.rb
+++ app/controllers/site_workers_controller.rb
@@ -1,35 +1,18 @@
 class SiteWorkersController < ApplicationController
   skip_before_filter :verify_authenticity_token
   skip_before_filter :require_login
-  MSG_TYPE={
-    "text"     =>  do_text,  #why can't it find the do_text func
-    "image"    =>  "fun_name",
-    "voice"    =>  "fun_name",
-    "video"    =>  "fun_name",
-    "location" =>  "fun_name",
-    "link"     =>  "fun_name",
-    "event"    =>  "fun_name"
-  }
 
-  EVENT_TYPE={
-    "subscribe"      =>  "fun_name",
-    "unsubscribe"    =>  "fun_name",
-    "SCAN"           =>  "fun_name",
-    "LOCATION"       =>  "fun_name",
-    "CLICK"          =>  "fun_name",
-    "VIEW"           =>  "fun_name"
-  }
 
   def wx_index
     render :text => params[:echostr]
   end
 	
   def wx_create
+    #client = WeechatClient.get_instance "siteworker"
     msg = params[:xml]
     fromUserName,toUserName = msg[:FromUserName],msg[:ToUserName]
     msg_type,create_time,mediaId,msgId = msg[:MsgType],Time.at(msg[:CreateTime].to_i),msg[:MediaId],msg[:MsgId]
-    p MSG_TYPE[msg_type]
-    #client = WeechatClient.get_instance "siteworker"
+    self.send "do_#{msg_type}",params
     #client.get_token if (client connect return 40001 which means the access_token is invalid)
     #client.download_media mediaId
   	@content = "hello site!"
@@ -44,8 +27,8 @@ class SiteWorkersController < ApplicationController
     render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
   end
 
-  def do_text
-    "~~#{params[:w]}--"
+  def do_text param
+    content=param[:Content]
   end
 
 
--- app/models/weechat_client.rb
+++ app/models/weechat_client.rb
@@ -1,22 +1,41 @@
 class WeechatClient
 	@@client_customer = nil
 	@@client_siteworker = nil
+	HOST_API = 'https://api.weixin.qq.com'
+	DOWNLOAD_DIR="/home/wjj/" #set the dir of the download file here
+	MAP={
+		"customer_appid"=>"wxea2ce7f47689a346",
+		"customer_secret"=>"6768e51d3dbd253e264de342847067d2",
+		"siteworker_appid"=>"",
+		"siteworker_secret"=>""
+	}
 
-	MAP={"customer_appid"=>"wxea2ce7f47689a346","customer_secret"=>"6768e51d3dbd253e264de342847067d2","siteworker_appid"=>"","siteworker_secret"=>""}
+	CLIENT_MAP={
+		"customer"=>@@client_customer,
+		"siteworker"=>@@client_siteworker
+	}
 
-	CLIENT_MAP={"customer"=>@@client_customer,"siteworker"=>@@client_siteworker}
-
-	def self.client_customer= customer_instance
-		@@client_customer=customer_instance
-	end
+	URL_ADVANCED = {
+    :user_info  => "/cgi-bin/user/info",
+    :menu       => "/cgi-bin/menu/create",
+    :menu_get   => "/cgi-bin/menu/get",
+    :user_list  => "/cgi-bin/user/get",
+    :group_list => "/cgi-bin/groups/get",
+    :user_change_group => "/cgi-bin/groups/members/update",
+    :send => "/cgi-bin/message/custom/send",
+    :media => "/cgi-bin/media/get"
+  }
+	# def self.client_customer= customer_instance
+	# 	@@client_customer=customer_instance
+	# end
 	
 	def self.client_customer
 		@@client_customer
 	end
 
-	def self.client_siteworker= site_work_instance
-		@@client_siteworker=site_work_instance
-	end
+	# def self.client_siteworker= site_work_instance
+	# 	@@client_siteworker=site_work_instance
+	# end
 	
 	def self.client_siteworker
 		@@client_siteworker
@@ -24,7 +43,7 @@ class WeechatClient
 
 	def self.get_instance name
 		CLIENT_MAP[name] = WeechatClient.new name if !CLIENT_MAP[name]
-		return CLIENT_MAP[name]
+		CLIENT_MAP[name]
 	end
 
 	def initialize type
@@ -33,7 +52,7 @@ class WeechatClient
 		@access_token = nil
 	end
 
-    def get_token
+    def get_api_token
       conn = Faraday.new(:url =>"http://file.api.weixin.qq.com") 
       response = conn.get "/cgi-bin/token?grant_type=client_credential" do |req|
         req.params['appid'] = @appid
@@ -42,10 +61,58 @@ class WeechatClient
       @access_token = JSON.parse(response.body)["access_token"]
     end
 
+    def api_get_user_list(next_openid=nil)
+      args = {}
+      args = {next_openid:next_openid} if next_openid
+      data = api_get(:user_list,args)
+      return nil unless data
+      data["data"]["openid"]
+    end
+
+    def api_get(url,args={})
+        fire_update = false
+        url = "#{URL_ADVANCED[url]}?"
+        args.each{|k,v| url = url + "&#{k.to_s}=#{v.to_s}"}
+    
+        if !@access_token then
+          get_api_token
+          return nil unless @access_token
+          fire_update = true
+        end
+    
+        conn = Faraday.new(:url =>@@host_api) 
+        response = conn.get url + "&access_token=#{@access_token}"
+        ret = JSON.parse(response.body)
+        if ret["errcode"] then #expired
+          p ret
+          if "42001"==ret["errcode"] then
+            return nil unless get_api_token
+            response = conn.get url + "&access_token=#{@access_token}"
+            fire_update = true
+          else
+            return nil
+          end
+        end
+        @update_block.call(@access_token) if (@update_block and fire_update)
+        return ret 
+    end
+
+	def api_post(url,body)
+	    get_api_token unless @access_token
+	    conn = Faraday.new(:url => @@host_api)
+	    response = conn.post "#{URL_ADVANCED[url]}?access_token=#{@access_token}" do |req|
+	      req.body = body
+	    end
+	    p response.body
+	    return (JSON.parse(response.body)["errcode"] == 0)
+	end
 	def download_media media_id
-		url="http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=#{@access_token}&media_id=#{media_id}"
+		res=api_get(:media,{"media_id"=>media_id})
+		open("#{DOWNLOAD_DIR}#{res.headers["content-disposition"].split("\"")[1]}","w+"){ |f| f.write(res.body)} if res	
 	end
 
 	def send_message
 	end
+
+
 end
