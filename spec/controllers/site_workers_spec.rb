07c52b2835b634586042ac88303f060a7b9b0be7
root <root@wjj-virtual-machine.(none)>
2014-03-27 16:54:56 +0800

    temp

---
+++ spec/controllers/site_workers_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe SiteWorkersController do
+	it "can download media" do
+	  	w = WeechatClient.get_instance "customer"
+	  	w.download_media '123'
+	  	#confirm the media is downloaded!
+	end
+end
