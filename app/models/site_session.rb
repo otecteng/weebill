e13543cc0224369c00e15c04de4226f245130af7
liteng <otec.teng@gmail.com>
2014-04-02 22:44:14 +0800

    message

--- app/models/site_session.rb
+++ app/models/site_session.rb
@@ -1,3 +1,4 @@
+#encoding: UTF-8
 class SiteSession < ActiveRecord::Base
   attr_accessible :site_worker_id,:status,:pix,:uid
   belongs_to :site_worker
@@ -28,7 +29,7 @@ class SiteSession < ActiveRecord::Base
     	param = transition.args[0]
     	service_order = ServiceOrder.find_by_id(param[:Content].to_i)
         if !service_order then
-            site_session.log "bad service order id #{param[:Content]}, check again?"
+            site_session.log "安装单编号错误： #{param[:Content]}, 请检查后重新输入"
            throw :halt
         else
             worker = site_session.site_worker
@@ -40,17 +41,17 @@ class SiteSession < ActiveRecord::Base
 
     state :pending do
         def message
-            "please upload a picture"  
+            "请上传施工图片"  
         end
     end
     state :pix_uploaded do
         def message
-            @message + "please submit a service order id"  
+            @message + "请输入安装单编号"  
         end
     end
     state :complished do
         def message
-            @message + "service order submitted,ainavi will confirm it as soon as possible" 
+            @message + "安装报告已提交，服务人员将尽快完成审核，谢谢！" 
         end
     end
   end
