048bb692f5020bebdf228c8e5c175124252f5431
Teng Li <otec.teng@gmail.com>
2014-04-29 17:56:28 +0800

    fix deploy bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
