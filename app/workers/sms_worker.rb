efe77b3c26a91d41f35f19a955e3a71cdca1705d
Teng Li <otec.teng@gmail.com>
2014-05-27 09:46:58 +0800

    hotfix for sms length

--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -2,10 +2,16 @@
 class SmsWorker
   include Sidekiq::Worker
   def perform(user_id,fans,msg)
-    ChinaSMS.use :smsbao, username:Setting.sms_user, password:Setting.sms_password
-    ret = ChinaSMS.to fans,msg
-    sms = SmsLog.create(:recv=>fans,:user_id=>user_id,:message=>msg,:amount=>(msg.length/64).ceil) # if ret[:success]
-    sms.status = ret[:success]
+  	vendor = Setting.sms_vendor.to_sym
+  	if vendor == :yunpian
+  		ChinaSMS.use :yunpian, password:Setting.sms_password
+  		ret = ChinaSMS.to fans,msg
+    else
+	    ChinaSMS.use vendor, username:Setting.sms_user, password:Setting.sms_password
+    	ret = ChinaSMS.to fans,msg
+    end
+    sms = SmsLog.create(:recv=>fans,:user_id=>user_id,:message=>msg,:amount=>(msg.length/64.0).ceil) # if ret[:success]
+    sms.status = ret[:msg]
     sms.save!
   end  
 end
